0f32d70d543044e59885fba35028c177e5c35b97
eduardo599 <eduardo599@gmail.com>
2021-09-16 12:25:47 -0500

    Sprint 3 - Documento Prefinalizado.

--- Base de datos Simat/Consultas Sql.sql
+++ Base de datos Simat/Consultas Sql.sql
@@ -3,3 +3,12 @@ update sedeeducativa set sedeEducativacol = "Saleciano", nid = "17" where idSede
 delete FROM sedeeducativa where idSedeEducativa = 10;
 
 select * FROM tablasimatsql.sedeeducativa;
+
+INSERT INTO tablasimatsql.estudiante (idUsuariosAlumnos, idSede, estrato, idAcudiente) VALUES (5, 1, "2", 5) ;
+INSERT INTO tablasimatsql.profesores (idUsuariosProfesores, usuario, clave) VALUES (4, "Johan55", "JO1234") ;
+
+select nombres, apellidos, tipoIdentificacion, fechaMatricula, jornada FROM matricula 
+inner join estudiante on  matricula.idEstudiante = estudiante.idAlumnos
+inner join usuario on estudiante.idUsuariosAlumnos = usuario.idUsuario
+inner join sedeeducativa on estudiante.idSede = sedeeducativa.idSedeEducativa
+where usuario.eps = "sanitas"
