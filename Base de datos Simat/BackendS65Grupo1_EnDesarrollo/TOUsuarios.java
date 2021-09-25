c2f70c6db530e2934405a2ec766dacd224ad561a
Geoleisha <90225035+Geoleisha@users.noreply.github.com>
2021-09-25 16:42:35 -0500

    TOUsuarios

---
+++ Base de datos Simat/BackendS65Grupo1_EnDesarrollo/TOUsuarios.java
@@ -0,0 +1,157 @@
+
+package TO;
+
+import java.sql.Date;
+
+
+public class TOUsuarios {
+    
+    private int  idUsuario;
+    private String  nombres;
+    private String  apellidos;
+    private String  tipoIdentificacion;
+    private String  identificacion;
+    private String  direccion;
+    private String  telefono;
+    private String  correo;
+    private String  genero;
+    private String  estadoCivil;
+    private Date fechaNacimiento;
+    private String  eps;
+    private String  tipoUsuario;
+
+    public TOUsuarios() {
+    }
+
+    public TOUsuarios(String nombres, String apellidos, String tipoIdentificacion, String identificacion) {
+        
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.tipoIdentificacion = tipoIdentificacion;
+        this.identificacion = identificacion;
+    }
+
+    public TOUsuarios(int idUsuario, String nombres, String apellidos, String tipoIdentificacion, String identificacion, String direccion, String telefono, String correo, String genero, String estadoCivil, Date fechaNacimiento, String eps, String tipoUsuario) {
+        
+       
+        this.idUsuario = idUsuario;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.tipoIdentificacion = tipoIdentificacion;
+        this.identificacion = identificacion;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.genero = genero;
+        this.estadoCivil = estadoCivil;
+        this.fechaNacimiento = fechaNacimiento;
+        this.eps = eps;
+        this.tipoUsuario = tipoUsuario;
+    }
+  
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(String tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEps() {
+        return eps;
+    }
+
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+    
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+ 
+}
