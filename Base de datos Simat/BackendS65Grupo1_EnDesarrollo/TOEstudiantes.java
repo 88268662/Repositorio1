73b49ad96d464f23c204fabc3d2bbd5cc12ad7ee
Geoleisha <90225035+Geoleisha@users.noreply.github.com>
2021-09-26 18:27:25 -0500

    TOEstudiantes

---
+++ Base de datos Simat/BackendS65Grupo1_EnDesarrollo/TOEstudiantes.java
@@ -0,0 +1,42 @@
+
+package TO;
+
+import java.sql.Date;
+
+public class TOEstudiantes extends TOUsuarios {
+    
+    private int idEstudiante;
+    private int idUsuariosEstudiante;
+    private int idSede;
+    private String estrato;
+    private int idAcudiente;
+    private String nombresAcudiente;
+    private String apellidosAcudiente;
+    private String sedeEducativaCol;
+    private String nid;
+    
+    
+
+    public TOEstudiantes() {
+    }
+
+    public TOEstudiantes(String nombres, String apellidos, String tipoIdentificacion, String identificacion) {
+        super(nombres, apellidos, tipoIdentificacion, identificacion);
+    }
+
+    public TOEstudiantes(int idEstudiante, int idUsuariosEstudiante, int idSede, String estrato, int idAcudiente, String nombresAcudiente, String apellidosAcudiente, String sedeEducativaCol, String nid, int idUsuario, String nombres, String apellidos, String tipoIdentificacion, String identificacion, String direccion, String telefono, String correo, String genero, String estadoCivil, Date fechaNacimiento, String eps, String tipoUsuario) {
+        super(idUsuario, nombres, apellidos, tipoIdentificacion, identificacion, direccion, telefono, correo, genero, estadoCivil, fechaNacimiento, eps, tipoUsuario);
+        this.idEstudiante = idEstudiante;
+        this.idUsuariosEstudiante = idUsuariosEstudiante;
+        this.idSede = idSede;
+        this.estrato = estrato;
+        this.idAcudiente = idAcudiente;
+        this.nombresAcudiente = nombresAcudiente;
+        this.apellidosAcudiente = apellidosAcudiente;
+        this.sedeEducativaCol = sedeEducativaCol;
+        this.nid = nid;
+    }
+
+
+    
+}
