487a13fc757b762a50589c029d0a3404690ecccc
NiniRodriguez <88518918+NiniRodriguez@users.noreply.github.com>
2021-09-26 17:41:43 -0500

    Creación archivo controller usuarios
    Se crea el archivo java controller usuarios

---
+++ Base de datos Simat/BackendS65Grupo1_EnDesarrollo/CtrlUsuarios.java
@@ -0,0 +1,19 @@
+
+package controllers;
+
+import DAO.DAOUsuarios;
+import TO.TOUsuarios;
+import java.util.ArrayList;
+
+
+public class CtrlUsuarios {
+    
+    DAOUsuarios usuariosDAO;
+
+    public CtrlUsuarios() {
+        this.usuariosDAO = new DAOUsuarios();
+    }
+    
+    
+    
+}
