fb3d4a9e6e60da2a184c3bd558cc5659c0bf5a78
alejogg10 <90224719+alejogg10@users.noreply.github.com>
2021-09-26 18:20:40 -0500

    Update CtrlUsuarios.java

--- Base de datos Simat/BackendS65Grupo1_EnDesarrollo/CtrlUsuarios.java
+++ Base de datos Simat/BackendS65Grupo1_EnDesarrollo/CtrlUsuarios.java
@@ -13,7 +13,21 @@ public class CtrlUsuarios {
     public CtrlUsuarios() {
         this.usuariosDAO = new DAOUsuarios();
     }
+public ArrayList<TOUsuarios> consultarUsuarios(){
+        return usuariosDAO.consultarUsuarios();
+    }
     
+    public int insertarUsuarios(TOUsuarios toUsuarios){
+        return usuariosDAO.insertarUsuarios(toUsuarios);
+    }
+    
+    public boolean actualizarUsuarios(TOUsuarios toUsuarios){
+        return usuariosDAO.actualizarUsuarios(toUsuarios);
+    }
+    
+    public boolean eliminarUsuarios(int idUsuarios){
+        return usuariosDAO.eliminarUsuarios(idUsuarios);
+    }    
     
     
 }
