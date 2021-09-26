ae7cc6535b04eb91858008615c40baa6c4ce164d
alejogg10 <90224719+alejogg10@users.noreply.github.com>
2021-09-26 18:46:32 -0500

    Update DAOUsuarios.java

--- Base de datos Simat/BackendS65Grupo1_EnDesarrollo/DAOUsuarios.java
+++ Base de datos Simat/BackendS65Grupo1_EnDesarrollo/DAOUsuarios.java
@@ -14,3 +14,36 @@ public class DAOUsuarios {
     private final String nombreTabla = "usuario";
   
   
+
+    
+     public ArrayList<TOUsuarios> consultarUsuarios(){
+
+        ArrayList<TOUsuarios> usuarios = new ArrayList<>();
+        TOUsuarios toUsuario;
+        try{
+            ResultSet rs = con.consultar(nombreTabla);
+            while (rs.next()){
+                toUsuario = new TOUsuarios();
+                toUsuario.setApellidos(rs.getString("apellidos"));
+                toUsuario.setCorreo(rs.getString("correo"));
+                toUsuario.setDireccion(rs.getString("direccion"));
+                toUsuario.setEps(rs.getString("eps"));
+                toUsuario.setEstadoCivil(rs.getString("estadoCivil"));
+                toUsuario.setFechaNacimiento(rs.getDate("fechaNacimiento"));
+                toUsuario.setGenero(rs.getString("genero"));
+                toUsuario.setIdUsuario(rs.getInt("idusuario"));
+                toUsuario.setIdentificacion(rs.getString("identificacion"));
+                toUsuario.setNombres(rs.getString("nombres"));
+                toUsuario.setTelefono(rs.getString("telefono"));
+                toUsuario.setTipoIdentificacion(rs.getString("tipoIdentificacion"));
+                toUsuario.setTipoUsuario(rs.getString("tipoUsuario"));
+                
+                usuarios.add(toUsuario);
+            }
+        return usuarios;
+        }catch(SQLException ex){
+            System.out.println("Error en DAOUsuarios.consultarUsuarios: " + ex.getMessage());
+            return null;
+        }
+    
+    }
