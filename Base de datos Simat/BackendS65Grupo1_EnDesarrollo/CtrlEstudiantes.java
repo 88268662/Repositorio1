8cacc281516b2e9b1088846cb0b0945dfab42988
NiniRodriguez <88518918+NiniRodriguez@users.noreply.github.com>
2021-09-26 17:53:26 -0500

    Creación archivo controller estudiantes
    se realiza la creación del archivo controller estudiantes

---
+++ Base de datos Simat/BackendS65Grupo1_EnDesarrollo/CtrlEstudiantes.java
@@ -0,0 +1,18 @@
+
+package controllers;
+
+import DAO.DAOEstudiantes;
+import TO.TOEstudiantes;
+import java.util.ArrayList;
+
+
+public class CtrlEstudiantes {
+    
+    DAOEstudiantes estudiantesDAO;
+
+    public CtrlEstudiantes() {
+        this.estudiantesDAO = new DAOEstudiantes();
+    }
+    
+      
+}
